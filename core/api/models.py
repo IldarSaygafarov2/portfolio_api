f13488a1423baa3bb24b3f52e60ac887d9ad4bb7
Ildar <sayildar17@gmail.com>
2023-04-15 20:53:39 +0500

    base configuration and models added

--- core/api/models.py
+++ core/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Service(models.Model):
+    name = models.CharField(verbose_name="Название услуги", max_length=100)
+    description = models.TextField(verbose_name="Описание услуги")
+    photo = models.ImageField(verbose_name="Фото", upload_to="services/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Member(models.Model):
+    name = models.CharField(verbose_name="Имя", max_length=100)
+    description = models.TextField(verbose_name="Описание")
+    service = models.ForeignKey(verbose_name="Услуга", to=Service, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
